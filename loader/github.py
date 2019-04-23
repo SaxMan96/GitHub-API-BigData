44b791d24cf8e44464895590ae4fffb35ddcb8b0
Michał Kośmider <mhl.kosmider@gmail.com>
2019-04-24 01:21:11 +0200

    Assignable users.

--- loader/github.py
+++ loader/github.py
@@ -3,6 +3,8 @@
 import re
 import requests
 
+from string import Template
+
 from loader import queries
 
 
@@ -71,6 +73,17 @@ def paginated(method):
     return node_generator
 
 
+def _selector(id_or_url):
+    if _is_id(id_or_url):
+        return 'node(id: "{}")'.format(id_or_url)
+    else:
+        return 'resource(url: "{}")'.format(id_or_url)
+
+
+def _cursor(cursor):
+    return 'null' if cursor is None else '"{}"'.format(cursor)
+
+
 class GitHub:
 
     def __init__(self, token, endpoint=None):
@@ -99,3 +112,39 @@ class GitHub:
         cursor = res['forks']['pageInfo']['endCursor']
         has_next = res['forks']['pageInfo']['hasNextPage']
         return results, cursor, has_next
+
+    def _get(self, id_or_url, query):
+        response = self.connection.query(query).json()
+
+        if 'errors' in response:
+            raise RuntimeError(response['errors'])
+
+        data = response['data']
+        if _is_id(id_or_url):
+            return data['node']
+        else:
+            return data['resource']
+
+    def __paginated(self, method, cursor=None):
+        cursor = cursor
+        has_next = True
+        while has_next:
+            nodes, cursor, has_next = method(cursor)
+            for result in nodes:
+                yield result
+
+    def _paginated(self, id_or_url, query, field, **kwargs):
+        def standard(cursor):
+            output = self._get(id_or_url, Template(query) \
+                .substitute(selector=_selector(id_or_url), cursor=_cursor(cursor), **kwargs))
+            nodes = output[field]['nodes']
+            cursor = output[field]['pageInfo']['endCursor']
+            has_next = output[field]['pageInfo']['hasNextPage']
+            return nodes, cursor, has_next
+        yield from self.__paginated(standard)
+
+    def get_repository_languages(self, id_or_url):
+        yield from self._paginated(id_or_url, queries.REPOSITORY_LANGUAGES, 'languages')
+
+    def get_repository_assignable_users(self, id_or_url):
+        yield from self._paginated(id_or_url, queries.REPOSITORY_ASSIGNABLE_USERS, 'assignableUsers')
