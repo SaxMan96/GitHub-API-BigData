32d117bdeb11d843fdd4377cc8590bb05232b190
Stanislaw Pawlak <stanislawpawlak3@gmail.com>
2019-05-13 02:14:29 +0200

    add: add assignable features

--- preparator/stats.py
+++ preparator/stats.py
@@ -2,6 +2,12 @@ import pandas as pd
 from gremlin_python.process.graph_traversal import GraphTraversal
 from gremlin_python.process.traversal import P
 
+COMPANY = 'company'
+
+BIO = "bio"
+ASSIGNABLE_PREFIX = 'assign_'
+ASSIGNABLE = "assignable"
+
 UNCLOSED_ISSUES = 'unclosed_issues'
 CLOSED = 'closed'
 CONTAINS = 'contains'
@@ -26,18 +32,27 @@ class Stats:
     def create_train_set(self, filename):
         repo_ids = self.g.V().has(TIME_PROCESSED, P.gt(0.0)).hasLabel(REPOSITORY).id().toList()
         print(f"{len(repo_ids)} ids downloaded...")
-        for i, repo_id in enumerate(repo_ids[:10]):
+        for i, repo_id in enumerate(repo_ids):
             self._create_repository_row(repo_id)
-            print(f"Repository {i} with id {repo_id} added...")
         self._save(filename)
 
     def _create_repository_row(self, repo_id):
         added = self._create_basic_df(repo_id)
         added = pd.concat([added,
                            self._add_language_features(repo_id),
-                           self._add_issue_features(repo_id)],
+                           self._add_issue_features(repo_id),
+                           self.add_assignable_features(repo_id)],
                           axis=1)
         self.df = pd.concat([self.df, added])
+
+    def add_assignable_features(self, repo_id):
+        assign_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).count().next()
+        bio_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).has(BIO).count().next()
+        company_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).has(COMPANY).count().next()
+        values = [assign_number, bio_number, company_number]
+        labels = [ASSIGNABLE_PREFIX, ASSIGNABLE_PREFIX + BIO, ASSIGNABLE_PREFIX + COMPANY]
+        return pd.DataFrame([values], columns=labels)
+
 
     def _create_basic_df(self, repo_id):
         properties = self.g.V(repo_id).properties().toList()
