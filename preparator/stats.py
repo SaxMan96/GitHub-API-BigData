a214c706692417a536f2e26142a9b9dd391b9461
Stanislaw Pawlak <stanislawpawlak3@gmail.com>
2019-05-13 02:46:19 +0200

    add: add stargazer features

--- preparator/stats.py
+++ preparator/stats.py
@@ -2,6 +2,8 @@ import pandas as pd
 from gremlin_python.process.graph_traversal import GraphTraversal
 from gremlin_python.process.traversal import P
 
+STARGAZER_PREFIX = 'stargazer_'
+STARGAZER = 'stargazer'
 COMPANY = 'company'
 
 BIO = "bio"
@@ -27,50 +29,72 @@ class Stats:
 
     def _create_main_dataframe(self):
         repo_id = self.g.V().has(TIME_PROCESSED, P.gt(0.0)).hasLabel(REPOSITORY).id().next()
+
         return pd.DataFrame(columns=self.g.V(repo_id).properties().label().toList())
 
     def create_train_set(self, filename):
         repo_ids = self.g.V().has(TIME_PROCESSED, P.gt(0.0)).hasLabel(REPOSITORY).id().toList()
+
         print(f"{len(repo_ids)} ids downloaded...")
+
         for i, repo_id in enumerate(repo_ids):
             self._create_repository_row(repo_id)
         self._save(filename)
 
     def _create_repository_row(self, repo_id):
         added = self._create_basic_df(repo_id)
+
         added = pd.concat([added,
                            self._add_language_features(repo_id),
                            self._add_issue_features(repo_id),
-                           self.add_assignable_features(repo_id)],
+                           self.add_assignable_features(repo_id),
+                           self.add_stargazer_features(repo_id)],
                           axis=1)
+
         self.df = pd.concat([self.df, added])
-
-    def add_assignable_features(self, repo_id):
-        assign_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).count().next()
-        bio_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).has(BIO).count().next()
-        company_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).has(COMPANY).count().next()
-        values = [assign_number, bio_number, company_number]
-        labels = [ASSIGNABLE_PREFIX, ASSIGNABLE_PREFIX + BIO, ASSIGNABLE_PREFIX + COMPANY]
-        return pd.DataFrame([values], columns=labels)
-
 
     def _create_basic_df(self, repo_id):
         properties = self.g.V(repo_id).properties().toList()
+
         labels = [p.label for p in properties]
         values = [p.value for p in properties]
+
         return pd.DataFrame([values], columns=labels)
 
     def _add_language_features(self, repo_id):
         # TODO: convert to map
         languages = self.g.V(repo_id).inE().hasLabel(USES).outV().path().by(NAME).by(SIZE).by(NAME).toList()
+
         labels = [language[2] for language in languages]
         sizes = [language[1] for language in languages]
+
         sizes = [size / sum(sizes) for size in sizes]
         return pd.DataFrame([sizes], columns=labels)
 
     def _add_issue_features(self, repo_id):
         n_of_unclosed_issues = self.g.V(repo_id).inE().hasLabel(CONTAINS).outV().has(CLOSED, False).count().toList()
+
         return pd.DataFrame([n_of_unclosed_issues], columns=[UNCLOSED_ISSUES])
+
+    def add_assignable_features(self, repo_id):
+        number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).count().next()
+        bio_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).has(BIO).count().next()
+        company_number = self.g.V(repo_id).inE().hasLabel(ASSIGNABLE).has(COMPANY).count().next()
+
+        values = [number, bio_number, company_number]
+        labels = [ASSIGNABLE_PREFIX, ASSIGNABLE_PREFIX + BIO, ASSIGNABLE_PREFIX + COMPANY]
+
+        return pd.DataFrame([values], columns=labels)
+
+    def add_stargazer_features(self, repo_id):
+        number = self.g.V(repo_id).inE().hasLabel(STARGAZER).count().next()
+        bio_number = self.g.V(repo_id).inE().hasLabel(STARGAZER).has(BIO).count().next()
+        company_number = self.g.V(repo_id).inE().hasLabel(STARGAZER).has(COMPANY).count().next()
+
+        values = [number, bio_number, company_number]
+        labels = [STARGAZER_PREFIX, STARGAZER_PREFIX + BIO, STARGAZER_PREFIX + COMPANY]
+
+        return pd.DataFrame([values], columns=labels)
 
     def _save(self, filename):
         self.df.to_csv(filename)
